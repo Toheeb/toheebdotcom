bc630a1722f4ac1b0df37f84e16d854ee2e448dc
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-09-14 03:32:40 +0100

    Include netlify.toml in build files

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/styles")
     eleventyConfig.addPassthroughCopy("_redirects")
+    eleventyConfig.addPassthroughCopy("netlify.toml")
 
     return {
         dir: {
