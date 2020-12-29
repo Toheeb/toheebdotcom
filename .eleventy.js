909952eaaae3970713a45135d20d09113ca13db1
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-12-29 06:57:03 +0100

    Structured layout and files

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("src/_theme/styles")
+    eleventyConfig.addPassthroughCopy("src/_theme/_assets")
     eleventyConfig.addPassthroughCopy("netlify.toml")
+
+    eleventyConfig.addLayoutAlias('post', '_page-post.njk')
 
     return {
         dir: {
             input: "src",
-            includes: "_theme/templates",
+            includes: "_theme/_includes",
         }
     }
 }
