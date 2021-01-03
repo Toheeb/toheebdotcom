298448199764171f9b08a478fbc8c9c93ce71483
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2021-01-03 15:58:54 +0100

    Reimplemented navigations using eleventy-navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/_theme/_assets")
     eleventyConfig.addPassthroughCopy("netlify.toml")
-
+    eleventyConfig.addPlugin(eleventyNavigationPlugin)
     eleventyConfig.addLayoutAlias('post', '_page-post.njk')
 
     return {
